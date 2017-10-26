6afa53fe65a9b3f3b0f2ee211c02b3640d720c24
lyxia <lysxia@gmail.com>
2017-10-26 09:31:46 -0400

    noninterf: Indistinguishability

--- noninterf/noninterf.c
+++ noninterf/noninterf.c
@@ -131,38 +131,129 @@ Outcome step(Machine *machine) {
   return STEPPED;
 }
 
-int main() {
-  Machine machine;
-  MemAtom memory[MEM_LENGTH];
-  StkAtom stack[STK_LENGTH];
-  Insn insns[PRG_LENGTH];
+Outcome run(Machine *machine) {
+  while (step(machine) == STEPPED) {
+    ;
+  }
+  return step(machine);
+}
 
-  klee_make_symbolic(&machine, sizeof machine, "machine");
-  klee_make_symbolic(&memory, sizeof memory, "memory");
-  klee_make_symbolic(&stack, sizeof stack, "stack");
-  klee_make_symbolic(&insns, sizeof insns, "insns");
+#define ASSERT(x) if (!(x)) { return 0; }
 
+int indist_atom(Atom a1, Atom a2) {
+  ASSERT(a1.tag == a2.tag);
+  if (a1.tag == L)
+    ASSERT(a1.value == a2.value);
+  return 1;
+}
+
+int indist_machine(Machine *m1, Machine *m2) {
+    ASSERT(m1->pc == m2->pc);
+    ASSERT(m1->sp == m2->sp);
+    for (int i = 0; i < m1->sp; i++) {
+      ASSERT(indist_atom(m1->stack[i], m2->stack[i]));
+    }
+    for (int i = 0; i < MEM_LENGTH; i++) {
+      ASSERT(indist_atom(m1->memory[i], m2->memory[i]));
+    }
+    for (int i = 0; i < PRG_LENGTH; i++) {
+      ASSERT(m1->insns[i].t == m2->insns[i].t);
+      if (m1->insns[i].t == PUSH)
+        ASSERT(indist_atom(m1->insns[i].immediate, m2->insns[i].immediate));
+    }
+    return 1;
+}
+
+void assume_valid_machine(Machine *machine) {
   // Why can't we use &&
-  klee_assume(0 <= machine.pc);
-  klee_assume(machine.pc < PRG_LENGTH);
-  klee_assume(0 <= machine.sp);
-  klee_assume(machine.sp < STK_LENGTH);
-  klee_assume(machine.memory == memory);
-  klee_assume(machine.stack == stack);
-  klee_assume(machine.insns == insns);
+  klee_assume(0 <= machine->pc);
+  klee_assume(machine->pc < PRG_LENGTH);
+  klee_assume(0 <= machine->sp);
+  klee_assume(machine->sp < STK_LENGTH);
 
-  for (int i = 0 ; i < machine.sp && i < STK_LENGTH ; i++) {
-    klee_assume(machine.stack[i].value >= 0);
+  for (int i = 0 ; i < machine->sp && i < STK_LENGTH ; i++) {
+    klee_assume(machine->stack[i].value >= 0);
+    klee_prefer_cex(machine->stack, machine->stack[i].value == 0);
   }
 
   for (int i = 0 ; i < MEM_LENGTH ; i++) {
-    klee_assume(machine.memory[i].value >= 0);
+    klee_assume(machine->memory[i].value >= 0);
+    klee_prefer_cex(machine->memory, machine->memory[i].value == 0);
   }
 
   for (int i = 0 ; i < PRG_LENGTH ; i++) {
-    klee_assume(machine.insns[i].immediate.value >= 0);
+    klee_assume(machine->insns[i].immediate.value >= 0);
+    klee_prefer_cex(machine->insns, machine->insns[i].t == NOOP);
+  }
+}
+
+void copy_machine(Machine* from, Machine *to) {
+  to->pc = from->pc;
+  to->sp = from->sp;
+  for (int i = 0; i < from->sp; i++) {
+    to->stack[i] = from->stack[i];
+  }
+  for (int i = 0; i < MEM_LENGTH; i++) {
+    to->memory[i] = from->memory[i];
+  }
+}
+
+int main() {
+  Machine machine1, machine1_, machine2, machine2_;
+  MemAtom
+    memory1[MEM_LENGTH], memory1_[MEM_LENGTH],
+    memory2[MEM_LENGTH], memory2_[MEM_LENGTH];
+  StkAtom
+    stack1[STK_LENGTH], stack1_[STK_LENGTH],
+    stack2[STK_LENGTH], stack2_[STK_LENGTH];
+  Insn insns1[PRG_LENGTH], insns2[PRG_LENGTH];
+
+  klee_make_symbolic(&machine1, sizeof machine1, "machine1");
+  klee_make_symbolic(&memory1, sizeof memory1, "memory1");
+  klee_make_symbolic(&stack1, sizeof stack1, "stack1");
+  klee_make_symbolic(&insns1, sizeof insns1, "insns1");
+
+  klee_make_symbolic(&machine2, sizeof machine2, "machine2");
+  klee_make_symbolic(&memory2, sizeof memory2, "memory2");
+  klee_make_symbolic(&stack2, sizeof stack2, "stack2");
+  klee_make_symbolic(&insns2, sizeof insns2, "insns2");
+
+  klee_assume(machine1.pc == 0);
+  klee_assume(machine1.memory == memory1);
+  klee_assume(machine1.stack == stack1);
+  klee_assume(machine1.insns == insns1);
+
+  klee_assume(machine2.memory == memory2);
+  klee_assume(machine2.stack == stack2);
+  klee_assume(machine2.insns == insns2);
+
+  assume_valid_machine(&machine1);
+
+  machine1_.memory = memory1_;
+  machine1_.stack = stack1_;
+  machine1_.insns = insns1;
+  copy_machine(&machine1, &machine1_);
+
+  // machine2_.memory = memory2_;
+  // machine2_.stack = stack2_;
+  // machine2_.insns = insns2;
+
+  if (run(&machine1_) == ERRORED) {
+    klee_silent_exit(0);
   }
 
-  step(&machine);
+  assume_valid_machine(&machine2);
+  if (!indist_machine(&machine1, &machine2)) {
+    klee_silent_exit(0);
+  }
+
+  if (run(&machine2) == ERRORED) {
+    klee_silent_exit(0);
+  }
+
+  if (!indist_machine(&machine1_, &machine2)) {
+    klee_abort();
+  }
+
   return 0;
 }
